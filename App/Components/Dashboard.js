5d9bc1014274c606621fa1abc7027fa0d76f24d2
dusan-dragas-vivify <dusan.dragas@vivifyideas.com>
2018-11-13 16:00:16 +0100

    "Add new" button implemented

--- App/Components/Dashboard.js
+++ App/Components/Dashboard.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { ActionButton } from 'react-native-material-ui';
 
 export default class Dashboard extends React.Component {
     render() {
         return (
-            <View>
+            <View style={styles.container}>
                 <Text>This is a dashboard page for our ToDo App!</Text>
+                <ActionButton/>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff'
+    }
+});
